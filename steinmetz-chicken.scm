c8376746d6f7e10d8452f21151b766d3b6d4cc38
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2023-01-04 13:15:21 -0500

    Add CHICKEN module and move imports from source.

---
+++ steinmetz-chicken.scm
@@ -0,0 +1,14 @@
+(module steinmetz
+  (option option-map option-add-arg-processor opt-help
+   opt-arg-names fold-cli parse-cli->alist)
+
+(import scheme
+        (chicken base)
+        (srfi 1)
+        (srfi 69)
+        (srfi 189)
+        )
+
+(include "string-util.scm")
+(include "options.scm")
+)
