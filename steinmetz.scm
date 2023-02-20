4a3cc70e7f385459ce7035cc8d5c9f8c11db2206
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2023-02-20 12:52:23 -0500

    Add R6RS library file.

---
+++ steinmetz.scm
@@ -0,0 +1,21 @@
+;;;; R6RS library file (for Chez Scheme; other R6 Schemes will
+;;;; need some adjustments).
+
+(library (steinmetz)
+  (export make-option option-map opt-help opt-arg-name fold-cli
+          process-cli make-usage options)
+  (import (rnrs base)
+          (rnrs control (6))
+          (rnrs lists (6))
+          (rnrs hashtables (6))
+          (only (srfi :1 lists) append-map)
+          (srfi :9 records)
+          (srfi :115)
+          (only (srfi :152) string-index string-skip string-drop-while
+                            string-concatenate string-join)
+          (only (chezscheme) include)
+          )
+  (include "command-line.scm")
+  (include "doc-portable.scm")
+  (include "options.scm")
+)
